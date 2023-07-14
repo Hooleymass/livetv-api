1f351cb5ea82be088bfe174c94e0842caa02d52d
Hooleymass <hockleymasese3@gmail.com>
2023-07-14 18:13:03 +0300

    server |\/

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ function extractChannels(html) {
         const url = links[name]; // get the URL for the current channel
 
         channels.push({
-	    'id': id,
+            'id': id,
             'name': name,
             'description': description,
             'thumbnail': thumbnail,
@@ -50,70 +50,71 @@ function extractChannels(html) {
     return channels;
 }
 
-// Route to display the links JSON object
+// Route to display the links as an array of objects
 app.get('/links', (req, res) => {
-    res.json(links);
+    const linkArray = Object.entries(links).map(([name, url]) => ({
+        name,
+        url
+    }));
+    res.json(linkArray);
 });
 
-// Route to display the social links JSON object
+// Route to display the social links as an array of objects
 app.get('/social', (req, res) => {
-    res.json(socialLinks);
+    const socialArray = Object.entries(socialLinks).map(([name, url]) => ({
+        name,
+        url
+    }));
+    res.json(socialArray);
 });
 
 // Route to display a specific channel by name
 app.get('/channels/:name', (req, res) => {
     const channelUrl = links[req.params.name];
     if (channelUrl) {
-        res.json({'name': req.params.name, 'url': channelUrl});
+        res.json({ 'name': req.params.name, 'url': channelUrl });
     } else {
-        res.status(404).json({'error': `Channel ${req.params.name} not found`});
+        res.status(404).json({ 'error': `Channel ${req.params.name} not found` });
     }
 });
 
-// Home route
-app.get('/', (req, res) => {
-    res.send('WELCOME TO MY LIVE TV API HOME<br><br>' +
-        '<a href="/links">Links</a><br>' +
-        '<a href="/social">Social Links</a><br>' +
-        '<a href="/channels">Channels</a>');
-});
-
+// Route to retrieve the channels data
 app.get('/channels', (req, res) => {
     request(url, (error, response, body) => {
         if (!error && response.statusCode == 200) {
             const channels = extractChannels(body);
-            res.json({'channels': channels});
+            res.json({ 'channels': channels });
         } else {
-            res.status(500).json({'error': 'Error occurred while extracting channels'});
+            res.status(500).json({ 'error': 'Error occurred while extracting channels' });
         }
     });
 });
 
-// Add route to update stream URLs
+// Route to update stream URLs
 app.put('/links', (req, res) => {
     const newData = req.body;
     links = newData;
     fs.writeFileSync(linkDataPath, JSON.stringify(newData));
-    res.json({'message': 'Stream URLs updated successfully'});
+    res.json({ 'message': 'Stream URLs updated successfully' });
 });
 
-// Add route to update social links
+// Route to update social links
 app.put('/social', (req, res) => {
     const newData = req.body;
     socialLinks = newData;
     fs.writeFileSync(socialDataPath, JSON.stringify(newData));
-    res.json({'message': 'Social links updated successfully'});
+    res.json({ 'message': 'Social links updated successfully' });
 });
 
-// Add route to add new channel
+// Route to add a new channel
 app.post('/channels', (req, res) => {
     const data = req.body;
     const name = data.name;
     const url = data.url;
     if (!name || !url) {
-        res.status(400).json({'error': 'Both name and URL are required to add a channel'});
+        res.status(400).json({ 'error': 'Both name and URL are required to add a channel' });
     } else if (name in links) {
-        res.status(400).json({'error': 'A channel with the same name already exists'});
+        res.status(400).json({ 'error': 'A channel with the same name already exists' });
     } else {
         links[name] = url;
         const newChannel = {
@@ -124,21 +125,22 @@ app.post('/channels', (req, res) => {
             'url': url
         };
         fs.writeFileSync(linkDataPath, JSON.stringify(links));
-        res.json({'message': 'Channel added successfully', 'newChannel': newChannel});
+        res.json({ 'message': 'Channel added successfully', 'newChannel': newChannel });
     }
 });
 
-// Add route to delete channel
+// Route to delete a channel
 app.delete('/channels/:name', (req, res) => {
     if (!(req.params.name in links)) {
-        res.status(404).json({'error': 'Channel not found'});
+        res.status(404).json({ 'error': 'Channel not found' });
     } else {
         delete links[req.params.name];
         fs.writeFileSync(linkDataPath, JSON.stringify(links));
-        res.json({'message': 'Channel deleted successfully'});
+        res.json({ 'message': 'Channel deleted successfully' });
     }
 });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
